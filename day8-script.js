06f96a16091102922037d4f49b86170849b68a73
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-02-28 20:57:41 +0700

    day 8 - Learn Javascript Function

---
+++ day8-script.js
@@ -0,0 +1,66 @@
+console.warn("Day 8 - Javascript Function")
+
+// Alert built function
+// alert("This is message");
+
+// Build function
+function greeting(name) {
+  return `Hi Programmers, my name is ${name}`;
+}
+console.log(greeting("Aidil Baihaqi"));
+
+function buildPerson(name, age, occupation) {
+  const person = {
+    name: name,
+    age: age,
+    occupation: occupation
+  };
+
+  return person;
+}
+const person = buildPerson("Aidil", 19, "Student");
+console.log(person);
+
+// logika ini belum selesai
+
+function seeGrade(name, score=0){
+  if(score>=90 && score<=100) {
+    console.log(`${name} get A grade`);
+  }else if(score>=80 && score<=90) {
+    console.log(`${name} get B grade`);
+  }else{
+    console.log(`${name} get C grade`);
+  }
+}
+
+seeGrade("Mas Aidil", 78)
+
+// OOP
+// Object Functions for OOP Javascript
+const people = {
+  name: "Oktaviandra",
+  age: 19, 
+  occupation: "Student",
+
+  // Method for displaying names
+  sayName() {
+    console.log(this.name);
+  }
+}
+people.sayName();
+
+// Instance and Constructor
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayName(){
+    console.log(this.name)
+  }
+}
+const fia = new Person("Fia", 18); 
+
+// belum selesai
+
